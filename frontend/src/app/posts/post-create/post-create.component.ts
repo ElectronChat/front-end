fc6e2fe0d14b4e537c64da63965e5242d93d226d
Philip Marshall <philipmarshall@Philips-MacBook-Pro.local>
2022-09-22 19:42:31 -0600

    More work on the angular tutorial

--- frontend/src/app/posts/post-create/post-create.component.ts
+++ frontend/src/app/posts/post-create/post-create.component.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 
 @Component ( {
   selector: 'app-post-create',
-  templateUrl: './post-create.component.html'
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
+  enteredValue = '';
   newPost = 'before post request';
 
 
   onAddPost() {
-    this.newPost = 'the user\'s post';
+
+    this.newPost = this.enteredValue;
   }
 }
