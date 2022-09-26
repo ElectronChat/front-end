1873e0800e78ffb215630f12bd2072c60bdecc50
Philip Marshall <philipmarshall@Philips-MacBook-Pro.local>
2022-09-26 10:00:09 -0600

    Continued work on the front end

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = "frontend";
+  storedPosts:{title: string, content: string}[] = [];
+
+  onPostAdded(post: { title: string; content: string; }) {
+    this.storedPosts.push(post);
+  }
 }
